cf9a29980e6ae79e3ae0d0f70f5fad7a66d936e3
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-05 16:49:40 +0530

    ArrayUtility Program

---
+++ ArrayUtilityProgram/ArrayUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayUtilityProgram
+{
+    public  class ArrayUtility
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+                int[] tempArray = new int[newSize];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    tempArray[i] = array[i];
+
+                }
+                array = tempArray;
+        }
+    }
+    
+}
---
+++ ArrayUtilityProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArrayUtilityProgram
+{
+    class program
+    {
+        public static void Main()
+        {
+            int[] numbers = new int[2];
+            numbers[0] = 10;
+            numbers[1] = 20;
+            ArrayUtility.Resize(ref numbers, numbers.Length + 2);
+            numbers[2] = 30;
+            numbers[3] = 40;
+            ArrayUtility.Resize(ref numbers, numbers.Length + 2);
+            numbers[4] = 50;
+            numbers[5] = 60;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
